297a59f163898fcee03d75c442f2f5a15f8929ad
plp_Ac_2024 <jeanbaron011@gmail.com>
2024-04-03 14:34:32 +0300

    Add num

---
+++ num.py
@@ -0,0 +1,29 @@
+#import numpy as np
+# Creating an array using np.array()method
+#arr = np.array([10, 20, 30, 40, 50])
+#Printing
+#print(arr)
+
+
+# more than one dimensions
+#import numpy as np
+#a = np.array([[1, 2], [3, 4], [5,6],[8,7],[9,10]])
+#print(a)
+
+
+# minimum dimensions
+#import numpy as np
+#a = np.array([1, 2, 3,4,5], ndmin=0)
+#print(a)
+
+
+# dtype parameter
+#import numpy as np
+#a = np.array([1, 2, 3], dtype = complex)
+#print(a)
+
+# dtype parameter
+#import numpy as np
+#a = np.array([1, 2, 3], dtype = bool)
+#print(a)
+
---
+++ numiterate.py
@@ -0,0 +1,16 @@
+#import numpy as np
+#a=np.arange(2,72,5)
+#a = a.reshape(7,2)
+#print('Original array is:')
+#print(a)
+#print('\n')
+#print('Modified array is:')
+#for x in np.nditer(a):
+  #print(x) 
+
+#import pandas as pd
+#import numpy as np
+#df = pd. DataFrame(np.random.randn(4,3),columns= ['col1','col2','col3'])
+#for row_index,row in df.iterrows():
+ # print(row_index,row)
+
